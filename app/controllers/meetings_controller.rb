1c5de0e3794d90ec76356b3612ed304aa0857007
Carlos <school@Carlos-MacBook-Air.local>
2017-03-06 20:07:08 -0800

    Added models and associations, starting to make it more dynamic as it is very basic

---
+++ app/controllers/meetings_controller.rb
@@ -0,0 +1,73 @@
+class MeetingsController < ApplicationController
+  def index
+    if params[:tag]
+      @meetings = Tag.find_by(name: params[:tag]).meetings
+    else
+      @meetings = Meeting.all
+    end
+    render 'index.html.erb'
+  end
+
+  def new
+    @meeting = Meeting.new
+    @tags = Tag.all # array of all our tags
+  end
+
+  def create
+    @meeting = Meeting.new(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes]
+      )
+    if @meeting.save
+      params[:tags].each do |tag_id| #array of tag ids
+        MeetingTag.create(
+          meeting_id: @meeting.id,
+          tag_id: tag_id
+          )
+      end
+      flash[:success] = "Meeting successfully created"
+      redirect_to "/meetings/#{@meeting.id}"
+    else
+      @tags = Tag.all
+      render :new
+    end
+  end
+
+  def show
+    @meeting = Meeting.find(params[:id])
+    render 'show.html.erb'
+  end
+
+  def edit
+    @meeting = Meeting.find( params[:id])
+    @tags = Tag.all
+    
+  end
+
+  def update
+    @meeting = Meeting.find(params[:id])
+    if @meeting.update(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes]
+      )
+      @meeting.tags.destroy_all
+      params[:tags].each do |tag_id| 
+        MeetingTag.create(
+          meeting_id: @meeting.id,
+          tag_id: tag_id
+          )
+      end
+      flash[:success] = "Meeting successfully updated"
+      redirect_to "/meetings/#{@meeting.id}"
+    else
+      @tags = Tag.all
+      render :edit
+    end
+  end
+end
